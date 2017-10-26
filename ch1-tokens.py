f0cf574651bd19c2a0511ccaec2f7f985dd71d92
lliverjose <lliver@live.com>
2017-10-25 20:33:05 -0400

    This file deals with tokens

---
+++ ch1-tokens.py
@@ -0,0 +1,83 @@
+import math
+
+'''
+Operators
++ - * ** / // % << >> & | ^ ~ < > <= => != <> += -= *= /= //= %= &= |= ^= >>= <<= **=
+'''
+i = 5
+j = 6
+
+print('i = ', i)
+print('j = ', j)
+print('i + j =', i + j)
+print('i - j =', i - j)
+print('i * j =', i * j)
+print('i ** j =', i ** j)
+print('i / j =', i / j)
+print('i // j =', i // j)
+print('i % j =', i % j)
+print('i << j =', i << j)
+print('i >> j =', i >> j)
+print('i & j =', i & j)
+print('i | j =', i | j)
+print('i ^ j =', i ^ j)
+
+print('i ~ j =', ~j)
+
+
+#print('i <> j =', (i <> j))
+print('i <= j =', i <= j)
+print('i >= j =', i >= j)
+print('i != j =', i != j)
+
+
+x = 5.0
+y = 6.0
+
+x += y
+print('x += y =', x)
+
+x -= y
+print('x -= y =', x)
+
+print('x *= y =',x)
+
+x /= y
+print('x /= y =', x)
+
+x //= y
+print('x //= y =', x)
+
+x %= y
+print('x %= y =', x)
+
+#x &= y
+print('x &= y =', x)
+
+#x |= y
+print('x |= y =', x)
+
+#x ^= y
+print('x ^= y =', x)
+
+#x >>= y
+print('x >>= y =', x)
+
+#x << y
+print('x <<= y =', x)
+
+#x **= y
+print('x **= y =', x)
+
+lower_with_under = 6
+
+total_balance = 5
+
+
+
+int_a = 7
+id(int_a)
+
+print(id(int_a))
+print(type(int_a))
+
